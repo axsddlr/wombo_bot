46c08cca73615a9755a4263d7107a21fce37ec0f
Andre Saddler <dresadd09691@gmail.com>
2022-05-25 09:45:43 -0400

    generator: update request endpoint and add refresh_token to payload data
    related to: https://github.com/axsddlr/wombo_bot/issues/3

--- utils/generator.py
+++ utils/generator.py
@@ -5,11 +5,13 @@ import requests
 
 async def gen_image(prompt: str, style):
     with requests.Session() as session:
+
+        data = "grant_type=refresh_token&refresh_token=AIwUaOmMdukSqJZSdchKpoLStrStqB7a1Uj9KfIDy8Z-mYnZPqXJlXRhZWy7SaOJPA2fKWX69oa04YqMLzVtXMnEvU_BYdxzij40PJke1wA8hGoo6OkWXgMpC-v5kT9M-q-B6Zwm4u61kzsubCyophH-nhOmKoeI6iUa1OnjOMrNouaVKVnfBOM"
+
         r = session.post(
-            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDCvp5MTJLUdtBYEKYWXJrlLzu1zuKM6Xw",
-            json={"returnSecureToken": True})
+            "https://securetoken.googleapis.com/v1/token?key=AIzaSyDCvp5MTJLUdtBYEKYWXJrlLzu1zuKM6Xw", data=data, headers={"Content-Type": "application/x-www-form-urlencoded"})
         data = r.json()
-        token = data["idToken"]
+        token = data["id_token"]
         auth_headers = {"Authorization": "bearer " + token}
 
         # retrieve task id
